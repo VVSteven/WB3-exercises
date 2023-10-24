6d2beb66b487fc7a8930254d1f0b6c087e93b60e
VVSteven <21elvisc@gmail.com>
2023-10-24 14:14:38 -0700

    finished simple functions

---
+++ Functions/simple_functions.js
@@ -0,0 +1,21 @@
+
+function favoriteThings()
+{
+console.log("Elvis");
+console.log("Cars");
+console.log("Destroy Lonely");
+}
+
+function whyImHere()
+{
+console.log("The Shekels To Buy The Porshe");
+}
+
+function favoritePlaces()
+{
+console.log("My favorite places to visit is my house, because it cool");
+}
+
+favoriteThings();
+whyImHere();
+favoritePlaces();
